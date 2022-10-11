05c86e720994de7c330c20e413309879a21f0284
JoshHyde9 <joshhyde546@gmail.com>
2022-10-11 14:48:06 +1100

    feat: ddate and time are functional

--- js/index.js
+++ js/index.js
@@ -7,3 +7,48 @@ footer.addEventListener("mouseenter", () => {
     footer.classList.remove("show");
   });
 });
+
+const clock = document.getElementById("clock");
+const date = document.getElementById("date");
+
+const createClock = () => {
+  const today = new Date();
+  const hours = today.getHours();
+  const minutes = today.getMinutes();
+
+  clock.innerText = `${hours}:${minutes}`;
+
+  setTimeout(() => {
+    createClock();
+  }, 1000);
+};
+
+const monthNames = [
+  "Jan",
+  "Febr",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
+const getDate = () => {
+  const today = new Date();
+  const month = today.getMonth();
+  const day = today.getDate();
+
+  date.innerText = `${monthNames[month]} ${day}`;
+
+  setTimeout(() => {
+    getDate();
+  }, 86400);
+};
+
+createClock();
+getDate();
